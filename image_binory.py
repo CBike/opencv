dbc53282290a849f708392d9b2d5d10023140db8
yangho.jeon <64768242+CBike@users.noreply.github.com>
2022-03-11 14:47:06 +0900

    opencv

--- image_binory.py
+++ image_binory.py
@@ -26,15 +26,20 @@ th = 150
 
 flag = [cv2.THRESH_BINARY, cv2.THRESH_MASK, cv2.THRESH_TOZERO, cv2.THRESH_TRUNC,
         cv2.THRESH_BINARY_INV, cv2.THRESH_TOZERO_INV, cv2.THRESH_TRIANGLE, cv2.THRESH_OTSU]
+
+flag_names = ['cv2.THRESH_BINARY', 'cv2.THRESH_MASK', 'cv2.THRESH_TOZERO', 'cv2.THRESH_TRUNC',
+             'cv2.THRESH_BINARY_INV', 'cv2.THRESH_TOZERO_INV', 'cv2.THRESH_TRIANGLE', 'cv2.THRESH_OTSU']
+
+
 cv2.imshow('bike',bike)
 cv2.imshow('greyscale',gray)
 
-for i in flag:
+
+for n, i in enumerate(flag):
     start = time.perf_counter()
     ret , result = cv2.threshold(gray, th, 255, i)
     finish = time.perf_counter()
     print(finish - start)
     print('threshold',ret)
-    cv2.imshow('{0}'.format(i), result)
-
+    cv2.imshow('{0}'.format(flag_names[n]), result)
 cv2.waitKey()
