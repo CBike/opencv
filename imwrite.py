3e5522dd19e2230ff7ec326e4af9254afa9a4f2f
yangho.jeon <64768242+CBike@users.noreply.github.com>
2022-03-15 10:59:49 +0900

    opencv

---
+++ imwrite.py
@@ -0,0 +1,13 @@
+import cv2
+
+bike = cv2.imread('./image/S1000rr.jpg', cv2.IMREAD_GRAYSCALE)
+
+cv2.imshow('bike',bike)
+k = cv2.waitKey(0)
+# k = cv2.waitKey(0) & 0xFF
+if k ==27 :
+    cv2.destroyAllWindows()
+
+elif k == ord('s'):
+    cv2.imwrite('./image/S1000rr_grayscale', bike)
+    cv2.destroyAllWindows()
