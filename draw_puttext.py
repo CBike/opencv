afd5559e4f40498276b553ca682b8e45dd6793b5
yangho.jeon <64768242+CBike@users.noreply.github.com>
2022-03-15 16:23:21 +0900

    opencv

---
+++ draw_puttext.py
@@ -0,0 +1,12 @@
+import numpy as np
+import cv2
+
+img = np.zeros((512,512,3), np.uint8)
+pts = np.array([[10,5], [20,30], [70,20], [50,10]], np.int32)
+pts = pts.reshape((-1, 1, 2))
+
+img = cv2.polylines(img, [pts], True, (0,255,0),20)
+img = cv2.putText(img,'Yangho.jeon',(10, 500), cv2.FONT_HERSHEY_PLAIN, 4, (0,0,255), 2)
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
