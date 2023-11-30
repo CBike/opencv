742773546700a29560c23d43c331bc73ff27b7d9
yangho.jeon <64768242+CBike@users.noreply.github.com>
2023-11-30 20:57:54 +0900

    openCV

---
+++ image Prymid.py
@@ -0,0 +1,14 @@
+import cv2
+
+src = cv2.imread("F:\dev\opencv\image\cat.jpg", cv2.IMREAD_COLOR)
+height, width, channel = src.shape
+
+dst = cv2.pyrUp(src, dstsize=(width * 2, height * 2), borderType=cv2.BORDER_DEFAULT)
+dst2 = cv2.pyrDown(src)
+
+cv2.imshow("src", src)
+cv2.imshow("dst", dst)
+cv2.imshow("dst2", dst2)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
