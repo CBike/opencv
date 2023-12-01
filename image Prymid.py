eaed3a23f7153ba1b6694efbdc8f6ebef42d8a1b
yangho.jeon <64768242+CBike@users.noreply.github.com>
2023-12-01 19:54:43 +0900

    openCV

--- image Prymid.py
+++ image Prymid.py
@@ -12,3 +12,26 @@ cv2.imshow("dst2", dst2)
 
 cv2.waitKey()
 cv2.destroyAllWindows()
+
+
+"""
+픽셀 외삽법 종류
+
+속성	의미
+cv2.BORDER_CONSTANT	    iiiiii | abcdefgh | iiiiiii
+cv2.BORDER_REPLICATE	aaaaaa | abcdefgh | hhhhhhh
+cv2.BORDER_REFLECT	    fedcba | abcdefgh | hgfedcb
+cv2.BORDER_WRAP	        cdefgh | abcdefgh | abcdefg
+cv2.BORDER_REFLECT_101	gfedcb | abcdefgh | gfedcba
+cv2.BORDER_REFLECT101	gfedcb | abcdefgh | gfedcba
+cv2.BORDER_DEFAULT	    gfedcb | abcdefgh | gfedcba
+cv2.BORDER_TRANSPARENT	uvwxyz | abcdefgh | ijklmno
+cv2.BORDER_ISOLATED	    관심 영역 (ROI) 밖은 고려하지 않음
+
+
+
+이미지 확대 함수는 BORDER_DEFAULT의 픽셀 외삽법만 사용할 수 있습니다.
+
+이미지 축소 함수는 BORDER_CONSTANT의 픽셀 외삽법을 제외한 나머지 플래그만 사용할 수 있습니다.
+
+"""
