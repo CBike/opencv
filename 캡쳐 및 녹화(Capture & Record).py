313c514e646c886aa4ce3a486064d8d9927cd65e
yangho.jeon <64768242+CBike@users.noreply.github.com>
2023-12-05 22:36:51 +0900

    openCV

---
+++ 캡쳐 및 녹화(Capture & Record).py
@@ -0,0 +1,44 @@
+import datetime
+import cv2
+
+capture = cv2.VideoCapture("./image/oppen.mp4")
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+record = False
+
+while True:
+    if (capture.get(cv2.CAP_PROP_POS_FRAMES) == capture.get(cv2.CAP_PROP_FRAME_COUNT)):
+        capture.open("./image/oppen.mp4")
+
+
+    ret, frame = capture.read()
+    cv2.imshow("VideoFrame", frame)
+
+    now = datetime.datetime.now().strftime("%d_%H_%M_%S")
+
+    key = cv2.waitKey(33)
+
+    # 27 = ESC, 26 = Ctrl + Z, 24 = Ctrl + X, 3 = Ctrl + C
+    if key == 27:
+        break
+
+    elif key == 26:
+        print("캡쳐")
+        cv2.imwrite("./image" + str(now) + ".png", frame)
+    elif key == 24:
+        print("녹화시작")
+        record = True
+        video = cv2.VideoWriter("./image" + str(now) + ".avi", fourcc, 20.0, (frame.shape[1], frame.shape[0]))
+
+    elif key == 3:
+        print("녹화 중지")
+        record = False
+        video.release()
+
+    if record == True:
+        print("녹화중..")
+        video.write(frame)
+
+
+
+capture.release()
+cv2.destroyAllWindows()
