90cf78cf6cae01ee174fa5bd0fa539d06dfb10c4
yangho.jeon <64768242+CBike@users.noreply.github.com>
2023-12-09 12:25:25 +0900

    openCV

--- ORB(Oriented FAST and Rotated BRIEF).py
+++ ORB(Oriented FAST and Rotated BRIEF).py
@@ -1,1 +1,35 @@
 import cv2
+import numpy as np
+
+src = cv2.imread("./image/apple.JPG")
+gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+target = cv2.imread ("./image/apple2.JPG", cv2.IMREAD_GRAYSCALE)
+
+
+orb = cv2.ORB_create(
+    nfeatures=40000,
+    scaleFactor=1.2,
+    nlevels=8,
+    edgeThreshold=31,
+    firstLevel=0,
+    WTA_K=2,
+    scoreType=cv2.ORB_HARRIS_SCORE,
+    patchSize=31,
+    fastThreshold=20
+)
+
+kp1, des1 = orb.detectAndCompute(gray, None)
+kp2, des2 = orb.detectAndCompute(target, None)
+
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+matches = bf.match(des1, des2)
+matches = sorted(matches, key=lambda x: x.distance)
+
+for i in matches[:100]:
+    idx = i.queryIdx
+    x1, y1 = kp1[idx].pt
+    cv2.circle(src, (int(x1), int(y1)), 3, (255, 0, 0), 3)
+
+
+cv2.imshow("src", src)
+cv2.waitKey()
---
+++ 마우스 콜백 (Mouse Callback).py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+
+
+def mouse_event(event, x, y, flags, param):
+    global radius
+
+    if event == cv2.EVENT_FLAG_LBUTTON:
+        cv2.circle(param, (x, y), radius, (255, 0, 0), 2)
+        cv2.imshow("draw", src)
+
+
+    elif event == cv2.EVENT_MOUSEWHEEL:
+        if flags > 0:
+            radius += 1
+
+        elif radius > 1:
+            radius-= 1
+
+
+
+
+radius = 3
+
+src = np.full((500, 500, 3), 255, dtype=np.uint8)
+
+cv2.imshow("draw", src)
+cv2.setMouseCallback("draw", mouse_event, src)
+cv2.waitKey()
