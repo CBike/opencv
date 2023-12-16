c3c5633c60de53b093689305fd4e23d65cfb2190
yangho.jeon <64768242+CBike@users.noreply.github.com>
2023-12-16 21:32:23 +0900

    openCV

--- cv2_diff.py
+++ cv2_diff.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 
 img1 = cv2.imread('./image/S1000rr.jpg',cv2.IMREAD_COLOR)
-img2 = cv2.imread('./image/S1000rr_grayscale.jpg',cv2.IMREAD_COLOR)
+img2 = cv2.imread('./image/S1000rr.jpg.jpg',cv2.IMREAD_COLOR)
 
 diff = cv2.absdiff(img1,img2)
 
